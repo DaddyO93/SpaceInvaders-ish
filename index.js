da4b58d9b543ff6289d0e7afd0fbb3da15903d04
Russ Lance <57551436+DaddyO93@users.noreply.github.com>
2021-01-11 21:25:59 -0600

    add sprites

--- index.js
+++ index.js
@@ -4,20 +4,26 @@ const c = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-// Player class
+// Player class using image
 class Player {
-  constructor(x, y, radius, color) {
+  constructor(x, y, playerImage, killBox) {
     this.x = x;
     this.y = y;
-    this.radius = radius;
-    this.color = color;
+    this.image = new Image();
+    this.image.src = playerImage;
+    this.killBox = killBox;
   }
+
   draw() {
-    c.beginPath();
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    c.fillStyle = this.color;
-    c.fill();
+    c.drawImage(
+      this.image,
+      this.x - this.killBox * 1.4,
+      this.y - this.killBox,
+      this.killBox * 3,
+      this.killBox * 3
+    );
   }
+
   update() {
     this.draw();
   }
@@ -25,33 +31,66 @@ class Player {
 
 // Enemy class
 class Enemy extends Player {
-  constructor(x, y, radius, color) {
-    super(x, y, radius, color);
+  constructor(x, y, enemyImage, killBox) {
+    super(x, y, enemyImage, killBox);
+    // this.x = x;
+    // this.y = y;
+    // this.image = new Image();
+    this.image.src = enemyImage;
+    // this.killBox = killBox;
   }
+
+  draw() {
+    c.drawImage(
+      this.image,
+      this.x - this.killBox * 1.4,
+      this.y - this.killBox,
+      this.killBox * 3,
+      this.killBox
+    );
+  }
+
   update() {
-    super.draw();
     this.y += enemyVertical;
     this.x = this.x + enemySpeed;
+    this.draw();
+    // super.draw();
   }
 }
 
 // Projectile class
-class Projectile extends Player {
-  constructor(x, y, radius, color, speed) {
-    super(x, y, radius, color);
+// class Projectile extends Player {
+class Projectile {
+  // constructor(x, y, killBox, color, speed) {
+  constructor(x, y, killBox, color, speed) {
+    // super(x, y, killBox);
+    this.x = x;
+    this.y = y;
+    this.killBox = killBox;
+    this.color = color;
     this.speed = speed;
   }
+
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+  }
+
   update() {
-    super.draw();
     this.y = this.y - this.speed;
+    this.x = this.x;
+    this.draw();
   }
 }
 
 // Particle class
-const friction = 0.98;
-class Particle extends Player {
-  constructor(x, y, radius, color, velocity) {
-    super(x, y, radius, color, velocity);
+const friction = 0.97;
+class Particle extends Projectile {
+  constructor(x, y, killBox, color, velocity) {
+    super(x, y, killBox, color);
+    this.velocity = velocity;
     this.alpha = 1;
   }
   // draws particle on screen
@@ -59,7 +98,7 @@ class Particle extends Player {
     c.save();
     c.globalAlpha = this.alpha;
     c.beginPath();
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.arc(this.x, this.y, this.killBox, 0, Math.PI * 2, false);
     c.fillStyle = this.color;
     c.fill();
     c.restore();
@@ -78,102 +117,265 @@ class Particle extends Player {
 const x = canvas.width;
 const y = canvas.height;
 
-let player = new Player(x / 2, y - 50, 10, "white", 0);
-let playerSpeed = 5000;
+let playerImage = [
+  "assets/ship.png",
+  "assets/shipLeft.png",
+  "assets/shipRight.png",
+];
+let playerKillBox = 50;
+let player = new Player(
+  x / 2 - playerKillBox,
+  y - 100,
+  playerImage[0],
+  playerKillBox,
+  "image"
+);
+
+let playerSpeed = 8;
 let playerProjectileSpeed = 8;
 let playerProjectileColor = "white";
+let playerProjectileSize = 3;
+let playerFireRate = 30;
+let fireTimer = 30;
+let playerProjectiles = [];
 
 let enemies = [];
+enemyImage = [
+  "assets/UFO1.png",
+  "assets/UFO2.png",
+  "assets/UFO3.png",
+  "assets/UFO4.png",
+];
+// let enemyImage = "assets/UFO1.png";
+let enemyKillbox = 50;
 let enemySpeed = 0.5;
 let enemyVertical = 0.05;
 let enemyCount = 12;
-let enemySize = 50;
 let enemyFireRate = 997;
-let enemyProjectileSpeed = -4;
-let enemyProjectileColor = "red";
+let enemyProjectileSpeed = -3;
+let enemyProjectileColor = "orange";
+let enemyProjectileSize = 3;
 let right = true;
+let enemyProjectiles = [];
 
-let projectiles = [];
-let projectileSize = 5;
-// let projectileColor = "white";
-// let projectileSpeed = 8;
+let particles = [];
 
+let keys = [];
+
+let index = 0;
+let projectileIndex = 0;
 let animationId;
 
 function init() {
-  player = new Player(x / 2, y - 50, 10, "white", 0);
-  playerSpeed = 10;
+  playerImage = [
+    "assets/ship.png",
+    "assets/shipLeft.png",
+    "assets/shipRight.png",
+  ];
+  playerKillBox = 20;
+  player = new Player(
+    x / 2 - playerKillBox,
+    y - 100,
+    playerImage[0],
+    playerKillBox,
+    "image"
+  );
+
+  playerSpeed = 8;
   playerProjectileSpeed = 8;
   playerProjectileColor = "white";
+  playerProjectileSize = 3;
+  playerFireRate = 30;
+  fireTimer = 30;
+  playerProjectiles = [];
 
   enemies = [];
+  enemyImage = [
+    "assets/UFO1.png",
+    "assets/UFO2.png",
+    "assets/UFO3.png",
+    "assets/UFO4.png",
+  ];
+  // enemyImage = "assets/UFO1.png";
+  enemyKillbox = 50;
   enemySpeed = 0.5;
   enemyVertical = 0.05;
   enemyCount = 12;
-  enemySize = 50;
   enemyFireRate = 997;
-  enemyProjectileSpeed = -4;
+  enemyProjectileSpeed = -3;
   enemyProjectileColor = "orange";
+  enemyProjectileSize = 3;
   right = true;
+  enemyProjectiles = [];
 
-  projectiles = [];
-  // projectileSpeed = 8;
-  projectileSize = 3;
-  // projectileColor = "white";
+  particles = [];
 
+  keys = [];
+
+  index = 0;
+  projectileIndex = 0;
   animationId;
 }
 
 function createEnemies() {
-  const radius = 50;
-
   let x = 150;
   let y = 150;
 
   //   place enemies in rows
   for (count = 0; count <= enemyCount; count++) {
-    if (x >= canvas.width - 150) {
-      y += 150;
-      x = 150;
+    if (x >= canvas.width - 200) {
+      y += 200;
+      x = 200;
     }
-    x += 150;
+    x += 200;
 
-    const color = `hsl(${Math.random() * 360}, 50%, 50%)`;
-
-    enemies.push(new Enemy(x, y, radius, color));
+    enemies.push(new Enemy(x, y, enemyImage[0], enemyKillbox, "image"));
   }
+}
+
+function controller() {
+  // move player left
+  if (keys["KeyA"] || keys["ArrowLeft"]) {
+    player.playerImage = playerImage[1];
+    player.x -= playerSpeed;
+  }
+
+  // move player right
+  if (keys["KeyD"] || keys["ArrowRight"]) {
+    player.playerImage = playerImage[2];
+    player.x += playerSpeed;
+  }
+
+  // player fire
+  if (keys["Space"]) {
+    if (fireTimer > playerFireRate) {
+      playerProjectiles.push(
+        new Projectile(
+          player.x,
+          player.y,
+          playerProjectileSize,
+          playerProjectileColor,
+          playerProjectileSpeed
+        )
+      );
+      fireTimer = 0;
+    }
+  }
+  fireTimer++;
 }
 
 // animation loop
 function animate() {
+  c.fillStyle = "rgba(0,0,0,1)";
+  c.fillRect(0, 0, canvas.width, canvas.height);
   animationId = requestAnimationFrame(animate);
 
-  c.fillStyle = "rgba(0,0,0,0.1)";
-  c.fillRect(0, 0, canvas.width, canvas.height);
+  controller();
 
   player.update();
   if (player.x <= 50) {
     player.x = 50;
-  } else if (player.x >= canvas.width - 50) {
-    player.x = canvas.width - 50;
+  } else if (player.x >= canvas.width - 100) {
+    player.x = canvas.width - 100;
   }
 
   // iterates through particles array & removes if faded out enough
-  // particles.forEach((particle, index) => {
-  //   if (particle.alpha <= 0) {
-  //     particles.splice(index, 1);
-  //   } else {
-  //     particle.update();
-  //   }
-  // });
+  particles.forEach((particle, index) => {
+    if (particle.alpha <= 0) {
+      particles.splice(index, 1);
+    } else {
+      particle.update();
+    }
+  });
 
-  projectiles.forEach((projectile, index) => {
-    // collision detection for enemy projectile and player
-    const dist = Math.hypot(player.x - projectile.x, player.y - projectile.y);
-    if (
-      dist - projectile.radius - player.radius < 1 &&
-      projectile.color === enemyProjectileColor
-    ) {
+  enemies.forEach((enemy, index) => {
+    // enemyImage =
+    // reverse direction when enemy reaches edge
+    if (enemy.x > canvas.width - 150 && right == true) {
+      enemySpeed *= -1;
+      right = false;
+    } else if (enemy.x < 150) {
+      enemySpeed *= -1;
+      right = true;
+    }
+    // end game if enemy reaches player Y
+    else if (enemy.y > canvas.height - 100) {
+      cancelAnimationFrame(animationId);
+    }
+
+    // iterate over player projectiles for hits
+    projectileIndex = 0;
+    playerProjectiles.forEach((projectile, projectileIndex) => {
+      // handle if enemy is hit
+      const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
+      if (dist - enemy.killBox - projectile.killBox < 1) {
+        // play sound when enemy is hit
+        // let explosionSound = new Audio('assets/explosion');
+        // explosionSound.volume = .6;
+        // explosionSound.play();
+
+        // creates explosions here
+        for (let i = 0; i < enemy.killBox; i++) {
+          particles.push(
+            new Particle(
+              projectile.x,
+              projectile.y,
+              Math.random() * 2,
+              enemy.color,
+              {
+                x: (Math.random() - 0.5) * (Math.random() * 10),
+                y: (Math.random() - 0.5) * (Math.random() * 10),
+              }
+            )
+          );
+        }
+
+        // shrink size of enemies when hit
+        if (enemy.killBox - 10 > 5) {
+          // adjusting score when hitting enemies and shrinking
+          // score += 100
+          // scoreElement.innerHTML = score
+
+          // "setTimeout" removes flashing when projectile is removed by waiting until next frame to removes projectile from array
+          setTimeout(() => {
+            playerProjectiles.splice(projectileIndex, 1);
+          }, 0);
+        } else {
+          // adjusting score when hitting enemies and destroying
+          // score += 250
+          // scoreElement.innerHTML = score
+
+          // removes flashing when enemy is removed by waiting until next frame and removes enemy and projectile from arrays
+          setTimeout(() => {
+            enemies.splice(index, 1);
+            playerProjectiles.splice(projectileIndex, 1);
+          }, 0);
+        }
+      }
+    });
+
+    // randomize enemy firerate and speed of projectiles
+    let fireFrequency = Math.random() * (1000 - 1) + 1;
+    let variableSpeed = Math.random() * 2;
+    if (fireFrequency > enemyFireRate) {
+      enemyProjectiles.push(
+        new Projectile(
+          enemy.x,
+          enemy.y,
+          enemyProjectileSize,
+          enemyProjectileColor,
+          enemyProjectileSpeed + variableSpeed
+        )
+      );
+    }
+    enemy.update();
+  });
+
+  projectileIndex = 0;
+  enemyProjectiles.forEach((projectile, projectileIndex) => {
+    // handle if player is hit
+    const dist = Math.hypot(projectile.x - player.x, projectile.y - player.y);
+    if (dist - player.killBox - projectile.killBox < 1) {
       // check for extra lives
       // if (extraLives <= 0) {
 
@@ -198,140 +400,49 @@ function animate() {
       //     loseLifeSound.play()
 
       //     // destroy enemy that touches player
-      //     enemy.radius - 60
+      //     enemy.killBox - 60
       //     enemies.splice(index, 1)
       //     extraLivesElement.innerHTML = extraLives
       // }
       // "setTimeout" removes flashing when projectile is removed by waiting until next frame to removes projectile from array
       setTimeout(() => {
-        projectiles.splice(index, 1);
+        enemyProjectiles.splice(projectileIndex, 1);
+      }, 0);
+    }
+    // remove projectile from array when leaves screen
+    if (projectile.y > canvas.height) {
+      setTimeout(() => {
+        enemyProjectiles.splice(projectileIndex, 1);
       }, 0);
     }
     projectile.update();
   });
 
-  enemies.forEach((enemy, index) => {
-    if (enemy.x > canvas.width - 150 && right == true) {
-      enemySpeed *= -1;
-      right = false;
-    } else if (enemy.x < 150) {
-      enemySpeed *= -1;
-      right = true;
+  playerProjectiles.forEach((projectile, projectileIndex) => {
+    // remove projectile from array when leaves screen
+    if (projectile.y < 0) {
+      setTimeout(() => {
+        playerProjectiles.splice(projectileIndex, 1);
+      }, 0);
     }
-
-    let number = Math.random() * (1000 - 1) + 1;
-    if (number > enemyFireRate) {
-      projectiles.push(
-        new Projectile(
-          enemy.x,
-          enemy.y,
-          projectileSize,
-          enemyProjectileColor,
-          enemyProjectileSpeed
-        )
-      );
-    }
-
-    // "HYPOT" = hypotenuse in this case refers to distance between two points
-    projectiles.forEach((projectile, projectileIndex) => {
-      const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
-
-      // collision detection loop and removal from array and screen for projectiles and enemies when projectile touches enemy
-      if (
-        dist - enemy.radius - projectile.radius < 1 &&
-        projectile.color === playerProjectileColor
-      ) {
-        // play sound when enemy is hit
-        // let explosionSound = new Audio('assets/explosion');
-        // explosionSound.volume = .6;
-        // explosionSound.play();
-
-        // creates explosions here
-        // for (let i = 0; i < enemy.radius * 2; i++) {
-        //   particles.push(
-        //     new Particle(
-        //       projectile.x,
-        //       projectile.y,
-        //       Math.random() * 2,
-        //       enemy.color,
-        //       {
-        //         x: (Math.random() - 0.5) * (Math.random() * 10),
-        //         y: (Math.random() - 0.5) * (Math.random() * 10),
-        //       }
-        //     )
-        //   );
-        // }
-
-        // shrink size of enemies when hit
-        if (enemy.radius - 10 > 5) {
-          // adjusting score when hitting enemies and shrinking
-          // score += 100
-          // scoreElement.innerHTML = score
-
-          // using gsap (GreenSock Animation Platform - a library) to transition in size smoothly
-          gsap.to(enemy, {
-            radius: enemy.radius - 10,
-          });
-
-          // "setTimeout" removes flashing when projectile is removed by waiting until next frame to removes projectile from array
-          setTimeout(() => {
-            projectiles.splice(projectileIndex, 1);
-          }, 0);
-        } else {
-          // adjusting score when hitting enemies and destroying
-          // score += 250
-          // scoreElement.innerHTML = score
-
-          // removes flashing when enemy is removed by waiting until next frame and removes enemy and projectile from arrays
-          setTimeout(() => {
-            enemies.splice(index, 1);
-            projectiles.splice(projectileIndex, 1);
-          }, 0);
-        }
-      }
-    });
-
-    enemy.update();
+    projectile.update();
   });
 }
 
-addEventListener(
-  "keydown",
-  (event) => {
-    if (event.defaultPrevented) {
-      return; // Do nothing if event already handled
-    }
-
-    switch (event.code) {
-      case "KeyA":
-      case "ArrowLeft":
-        // Handle "move left"
-        gsap.to(player, { x: (player.x -= playerSpeed) });
-        break;
-      case "KeyD":
-      case "ArrowRight":
-        // Handle "move right"
-        gsap.to(player, { x: (player.x += playerSpeed) });
-        break;
-      case "Space":
-        // Handle "fire"
-        projectiles.push(
-          new Projectile(
-            player.x,
-            player.y,
-            projectileSize,
-            playerProjectileColor,
-            playerProjectileSpeed
-          )
-        );
-        break;
-    }
-
-    // Consume the event so it doesn't get handled twice
-    event.preventDefault();
-  },
-  true
-);
+addEventListener("keydown", (event) => {
+  keys[event.code] = true;
+});
+addEventListener("keyup", (event) => {
+  keys[event.code] = false;
+  if (
+    event == "KeyA" ||
+    event == "KeyD" ||
+    event == "ArrowLeft" ||
+    event == "ArrowRight"
+  ) {
+    player.playerImage = "assets/ship.png";
+  }
+});
 
 init();
 animate();
